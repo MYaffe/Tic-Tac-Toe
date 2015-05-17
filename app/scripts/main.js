fe855c4b2296e776ad5d357e99375ab47030c4c7
Mendel Yaffe <shtusmail@gmail.com>
2015-05-17 16:18:24 -0400

    ric's bux fixes complete

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,36 +9,36 @@ var eight = $('#8');
 var nine = $('#9');
 
 
-function assignPlayerToSquare() {
-  //if (isAnswerRight=="yes"){
-    if (this==one){
-    (one).html(player)
-    console.log("one was clicked")
+function assignPlayerToSquare(id) {
+  console.log(id);
+  if (id == '1') {
+    return (one).html(player)
   }
-  if (this==two){
-    (two).html(player)
+  if (id=='2'){
+    return(two).html(player)
   } 
-  if (this==three){
-    (three).html(player)
+  if (id=='3'){
+    return(three).html(player)
   }
-   if (this==four){
-    (four).html(player)
+   if (id=='4'){
+    return(four).html(player)
   }
-   if (this==five){
-    (five).html(player)
+   if (id=='5'){
+   return (five).html(player)
   }
-   if (this==six){
-    (six).html(player)
+   if (id=='6'){
+    return(six).html(player)
   }
-   if (this==seven){
-    (seven).html(player)
+   if (id=='7'){
+    return(seven).html(player)
   }
-   if (this==eight){
-    (eight).html(player)
+   if (id=='8'){
+    return(eight).html(player)
   }
-   if (this==nine){
-    (nine).html(player)
-  }}
+   if (id=='9'){
+    return(nine).html(player)
+  }
+}
 
 
   function togglePlayer() {
@@ -66,73 +66,79 @@ function checkForWinner() {
     console.log('falsey');
   }}
 
-var num;
+
 function getRandomInt(min, max) {
-num=Math.floor(Math.random() * (max - min)) + min;
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
 $('.btn').click(function() {
-        getRandomInt(1, 6);
-        if (num>0&&num<3) {
-        return myPrompt(num);
-    }
-    if (num>2&&num<7) {
-        return myPromptTwo(num);
-    }
-  });
+  var id = this.id;
+  var num = getRandomInt(1, 7);
 
+  if (num>0&&num<4) {
+    return myPrompt(id);
+  }
+  else if (num>5&&num<8) {
+    return myPromptTwo(id);
+  }
+});
 
 var isAnswerRight;
 var person;  
 var player = 'X'; 
-function myPrompt(num) {
-    person = prompt("Please enter your name", "Harry Potter");
-    if (person == "Joe") {
-        //document.getElementById("demo").innerHTML =
-       // "Hello " + person + "! How are you today?";
-        isAnswerRight="yes";
-                console.log(isAnswerRight)
-                console.log(player)
 
-            $('#turns').html(player);
-            $('.btn').click(function() {
-            $(this).html(player)
-         //the last line doesn't make the statement "one.html()=x/o" true. Strange...
-            assignPlayerToSquare();
-            checkForWinner();
-            togglePlayer();
+function myPrompt(id) {
+  person = prompt("Please enter your name", "Harry Potter");
 
-  //$('#turns').html(player);
-});
- }
+  if (person == "Joe") {
+      //document.getElementById("demo").innerHTML =
+     // "Hello " + person + "! How are you today?";
+      // isAnswerRight="yes";
+      //         console.log(isAnswerRight)
 
-        else   {
-          isAnswerRight="no"
-        console.log(isAnswerRight)
+      //     $('#turns').html(player);
+      //     $('.btn').click(function() {
+      //     $(this).html(player)
+       //the last line doesn't make the statement "one.html()=x/o" true. Strange...
+    assignPlayerToSquare(id);
+          togglePlayer();
+          checkForWinner();
 
-        }
-    }
+
+//$('#turns').html(player);
+//});
+  }
+  else   {
+  //   isAnswerRight="no"
+  // console.log(isAnswerRight)
+
+  }
+}
 
 
 var person;   
-function myPromptTwo(num) {
+function myPromptTwo(id) {
     person = prompt("Please enter your name", "Mendel The Man");
     if (person == "Moe") {
-       // document.getElementById("demo").innerHTML =
-       // "Hello " + person + "Prompt number 2? Awesome!";
-             isAnswerRight="yes";
-                     console.log(isAnswerRight)
+        assignPlayerToSquare(id);
+          togglePlayer();
+          checkForWinner();
 
-            $('#turns').html(player);
-            $('.btn').click(function() {
-            $(this).html(player)
-         //the last line doesn't make the statement "one.html()=x/o" true. Strange...
-            assignPlayerToSquare();
-            checkForWinner();
-            togglePlayer();
+       // // document.getElementById("demo").innerHTML =
+       // // "Hello " + person + "Prompt number 2? Awesome!";
+       //       isAnswerRight="yes";
+       //               console.log(isAnswerRight)
+
+       //     // $('#turns').html(player);
+       //      $('.btn').click(function() {
+       //      //$(this).html(player)
+       //   //the last line doesn't make the statement "one.html()=x/o" true. Strange...
+       //      assignPlayerToSquare();
+       //      checkForWinner();
+       //      togglePlayer();
 
   //$('#turns').html(player);
-});
+//});
  
       }
 
